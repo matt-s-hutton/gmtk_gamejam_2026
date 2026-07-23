1c53d1cbe3af822a8ae81b11ce7b9bc9ed83f0e2
lynn <tharlynnhtet.ong@gmail.com>
2026-07-23 13:36:22 -0700

    Multiple insturment support

---
+++ src/music_manager/song.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name Song
+
+@export var metronone: AudioStream
+@export var bass: AudioStream
+@export var drums: AudioStream
+@export var keys: AudioStream
+@export var lead: AudioStream
+@export var strings: AudioStream
--- src/rhythm_conductor/rhythm_conductor.gd
+++ src/rhythm_conductor/rhythm_conductor.gd
@@ -1,4 +1,5 @@
 extends Node
+clas
 
 @export var current_song: AudioStream
 
